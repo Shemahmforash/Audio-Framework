c05a27e88371ac627d4e72806bafda1aa266690d
Shemahmforash <shemahmforash@gmail.com>
2011-08-31 14:14:28 +0100

    created an enumerator to keep the available onset detection methods

---
+++ src/com/icdif/audio/analysis/OnsetMethodology.java
@@ -0,0 +1,9 @@
+package com.icdif.audio.analysis;
+/**
+ * An enumerator with the available onset detection methods
+ * @author wanderer
+ *
+ */
+public enum OnsetMethodology {
+	SpectralFlux, PhaseDeviation, WeightedPhaseDeviation
+}
